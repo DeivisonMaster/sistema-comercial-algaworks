84557d6dc174934021e4f4074c4d64640dde50bf
deivisonmaster <deivisonoliveira.info@gmail.com>
2019-08-17 14:11:41 -0400

    Implementando produtor de entity manager

--- src/main/java/br/com/pedidovenda/controller/CadastroProdutoController.java
+++ src/main/java/br/com/pedidovenda/controller/CadastroProdutoController.java
@@ -1,10 +1,16 @@
 package br.com.pedidovenda.controller;
 
 import java.io.Serializable;
+import java.util.Collection;
 
 import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
+import br.com.pedidovenda.model.Categoria;
 import br.com.pedidovenda.model.Produto;
 
 @Named("cadastroProduto")
@@ -14,9 +20,22 @@ public class CadastroProdutoController implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	private Produto produto;
+	private Collection<Categoria> categorias;
+	
+	@Inject
+	private EntityManager entityManager;
+	
+	
 	
 	public CadastroProdutoController() {
 		this.produto = new Produto();
+	}
+	
+	public void inicializar() {
+		
+		
+		this.categorias = entityManager.createQuery("from Categoria",  Categoria.class).getResultList();
+		entityManager.close();
 	}
 
 	
@@ -30,5 +49,8 @@ public class CadastroProdutoController implements Serializable{
 	}
 	public void setProduto(Produto produto) {
 		this.produto = produto;
+	}
+	public Collection<Categoria> getCategorias() {
+		return categorias;
 	}
 }
---
+++ src/main/java/br/com/pedidovenda/util/JPAUtil.java
@@ -0,0 +1,35 @@
+package br.com.pedidovenda.util;
+
+import java.io.Serializable;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.context.RequestScoped;
+import javax.enterprise.inject.Disposes;
+import javax.enterprise.inject.Produces;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+@ApplicationScoped
+public class JPAUtil implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private EntityManagerFactory entityManagerFactory; 
+	
+	
+	public JPAUtil() {
+		entityManagerFactory = Persistence.createEntityManagerFactory("PedidoVenda");
+	}
+	
+	@Produces
+	@RequestScoped
+	public EntityManager getEntityManager() {
+		return entityManagerFactory.createEntityManager();
+	}
+	
+	public void closeEntityManager(@Disposes EntityManager entityManager) {
+		entityManager.close();
+	}
+	
+	
+}
