ae87998ad2d66c3819bcc9fd8a060015b4c16fa1
deivisonmaster <deivisonoliveira.info@gmail.com>
2019-11-02 20:23:21 -0400

    exibindo o usuario logado

---
+++ src/main/java/br/com/pedidovenda/security/Seguranca.java
@@ -0,0 +1,39 @@
+package br.com.pedidovenda.security;
+
+import javax.enterprise.context.RequestScoped;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+
+@Named
+@RequestScoped
+public class Seguranca {
+	
+	
+	public String getNomeUsuario() {
+		String nome = null;
+		
+		UsuarioSistema usuarioLogado = getUsuarioLogado();
+		
+		if(usuarioLogado !=null ) {
+			nome = usuarioLogado.getUsuario().getNome();
+		}
+		
+		return nome;
+	}
+
+	private UsuarioSistema getUsuarioLogado() {
+		UsuarioSistema usuario = null;
+		
+		// recupera o usuario logado
+		UsernamePasswordAuthenticationToken auth = (UsernamePasswordAuthenticationToken)
+				FacesContext.getCurrentInstance().getExternalContext().getUserPrincipal();
+		
+		if(auth != null && auth.getPrincipal() != null) {
+			usuario = (UsuarioSistema) auth.getPrincipal();
+		}
+		
+		return usuario;
+	}
+}
